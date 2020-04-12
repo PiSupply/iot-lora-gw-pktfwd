8544725795333b0cad6c94dd77b1cda2d138517e
Ryan Walmsley <ryanteck@gmail.com>
2020-04-12 21:25:00 +0100

    Dev

--- files/run_pkt.sh
+++ files/run_pkt.sh
@@ -3,4 +3,9 @@
 #Run Packetforward
 #This script runs on the boot of the container
 
-python3 -u configurePktFwd.py
+#python3 -u configurePktFwd.py
+
+#Lets see if we can bodge
+./reset-38.sh
+./reset-39.sh
+./packetforwarder_sg0 && ./packetforwarder_sg1
