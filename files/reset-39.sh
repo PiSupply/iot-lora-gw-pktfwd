dc334cb25c5d72634aad6ef88fc221b2a11a95ce
Ryan Walmsley <ryanteck@gmail.com>
2020-04-12 21:17:22 +0100

    Longer delays?

--- files/reset-39.sh
+++ files/reset-39.sh
@@ -9,3 +9,7 @@ echo "1" > /sys/class/gpio/gpio39/value
 sleep 2
 echo "0" > /sys/class/gpio/gpio39/value
 sleep 2
+echo "1" > /sys/class/gpio/gpio39/value
+sleep 2
+echo "0" > /sys/class/gpio/gpio39/value
+sleep 2
